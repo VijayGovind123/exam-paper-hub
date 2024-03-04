86587bbcb1a6d6756cfed376f221e2ad631b74a1
Ande Kishore <120158003+VijayGovind123@users.noreply.github.com>
2024-03-04 17:59:43 +0530

    Update RentModal.tsx

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -175,43 +175,43 @@ const RentModal = () => {
     );
   }
 
-  if (step === STEPS.INFO) {
-    bodyContent = (
-      <div className="flex flex-col gap-8">
-        <Heading
-          title="Share some basics about your place"
-          subtitle="What amenitis do you have?"
-        />
-        <Counter 
-          onChange={(value) => setCustomValue('guestCount', value)}
-          value={guestCount}
-          title="Guests" 
-          subtitle="How many guests do you allow?"
-        />
-        <hr />
-        <Counter 
-          onChange={(value) => setCustomValue('roomCount', value)}
-          value={roomCount}
-          title="Rooms" 
-          subtitle="How many rooms do you have?"
-        />
-        <hr />
-        <Counter 
-          onChange={(value) => setCustomValue('bathroomCount', value)}
-          value={bathroomCount}
-          title="Bathrooms" 
-          subtitle="How many bathrooms do you have?"
-        />
-      </div>
-    )
-  }
+  // if (step === STEPS.INFO) {
+  //   bodyContent = (
+  //     <div className="flex flex-col gap-8">
+  //       <Heading
+  //         title="Share some basics about your place"
+  //         subtitle="What amenitis do you have?"
+  //       />
+  //       <Counter 
+  //         onChange={(value) => setCustomValue('guestCount', value)}
+  //         value={guestCount}
+  //         title="Guests" 
+  //         subtitle="How many guests do you allow?"
+  //       />
+  //       <hr />
+  //       <Counter 
+  //         onChange={(value) => setCustomValue('roomCount', value)}
+  //         value={roomCount}
+  //         title="Rooms" 
+  //         subtitle="How many rooms do you have?"
+  //       />
+  //       <hr />
+  //       <Counter 
+  //         onChange={(value) => setCustomValue('bathroomCount', value)}
+  //         value={bathroomCount}
+  //         title="Bathrooms" 
+  //         subtitle="How many bathrooms do you have?"
+  //       />
+  //     </div>
+  //   )
+  // }
 
   if (step === STEPS.IMAGES) {
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading
-          title="Add a photo of your place"
-          subtitle="Show guests what your place looks like!"
+          title="Add a photo of your Paper"
+          subtitle="Upload good quality photo"
         />
         <ImageUpload
           onChange={(value) => setCustomValue('imageSrc', value)}
@@ -225,7 +225,7 @@ const RentModal = () => {
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading
-          title="How would you describe your place?"
+          title="Name of the subject"
           subtitle="Short and sweet works best!"
         />
         <Input
@@ -254,7 +254,7 @@ const RentModal = () => {
       <div className="flex flex-col gap-8">
         <Heading
           title="Now, set your price"
-          subtitle="How much do you charge per night?"
+          subtitle="How much do you charge per Paper?"
         />
         <Input
           id="price"
